31d6352ea55f4f6a5c55d189f31b7ad6d9bdde61
habeebu <habeebu@hello24.in>
2024-02-17 21:09:42 +0530

    completed

--- lib/views/auth/login_screen.dart
+++ lib/views/auth/login_screen.dart
@@ -13,92 +13,94 @@ class LoginScreen extends GetView<AuthController> {
       backgroundColor: const Color(0xffB7DACA),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: Get.height * 0.1),
-            const Text(
-              'Welcome Back!',
-              style: TextStyle(
-                  fontSize: 30.0,
-                  color: Color(0xff21A177),
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: Get.height * 0.02),
-            const Text(
-              'Stay Signed in with your account to make it easier',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: Get.height * 0.02),
-            const Image(
-              image: AssetImage('assets/icon.png'),
-              fit: BoxFit.cover,
-              height: 200,
-              width: 200,
-            ),
-            SizedBox(height: Get.height * 0.05),
-            TextField(
-              controller: controller.mobileControllerLogin,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.phone),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(color: Color(0xffE6FBDA)),
-                ),
-                labelText: 'Mobile',
-                prefixIconColor: const Color(0xff20A479),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: Get.height * 0.1),
+              const Text(
+                'Welcome Back!',
+                style: TextStyle(
+                    fontSize: 30.0,
+                    color: Color(0xff21A177),
+                    fontWeight: FontWeight.bold),
               ),
-            ),
-            SizedBox(height: Get.height * 0.05),
-            controller.loading.value
-                ? const Center(
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.orange,
-                      strokeWidth: 4,
-                    ),
-                  )
-                : SizedBox(
-                    height: 50,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        controller.fetchLogin();
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(const Color(0xff41AC1D)),
+              SizedBox(height: Get.height * 0.02),
+              const Text(
+                'Stay Signed in with your account to make it easier',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(height: Get.height * 0.02),
+              const Image(
+                image: AssetImage('assets/icon.png'),
+                fit: BoxFit.cover,
+                height: 200,
+                width: 200,
+              ),
+              SizedBox(height: Get.height * 0.05),
+              TextField(
+                controller: controller.mobileControllerLogin,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.phone),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(color: Color(0xffE6FBDA)),
+                  ),
+                  labelText: 'Mobile',
+                  prefixIconColor: const Color(0xff20A479),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.05),
+              controller.loading.value
+                  ? const Center(
+                      child: CircularProgressIndicator(
+                        backgroundColor: Colors.orange,
+                        strokeWidth: 4,
                       ),
-                      child: const Text(
-                        'Sign In',
-                        style: TextStyle(fontSize: 18),
+                    )
+                  : SizedBox(
+                      height: 50,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          controller.fetchLogin();
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              const Color(0xff41AC1D)),
+                        ),
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Don\'t have an account?',
+                    style: TextStyle(color: Color(0xff61B046), fontSize: 18),
+                  ),
+                  SizedBox(height: Get.height * 0.05),
+                  InkWell(
+                    onTap: () {
+                      Get.to(() => const RegisterScreen());
+                    },
+                    child: const Text(
+                      ' Sign Up',
+                      style: TextStyle(
+                        color: Color(0xff23A477),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
                       ),
                     ),
                   ),
-            const SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Don\'t have an account?',
-                  style: TextStyle(color: Color(0xff61B046), fontSize: 18),
-                ),
-                SizedBox(height: Get.height * 0.05),
-                InkWell(
-                  onTap: () {
-                    Get.to(() => const RegisterScreen());
-                  },
-                  child: const Text(
-                    ' Sign Up',
-                    style: TextStyle(
-                      color: Color(0xff23A477),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/views/auth/register_screen.dart
+++ lib/views/auth/register_screen.dart
@@ -15,106 +15,108 @@ class RegisterScreen extends GetView<AuthController> {
       backgroundColor: const Color(0xffB7DACA),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: Get.height * 0.2),
-            const Text(
-              'Register to get started',
-              style: TextStyle(
-                  fontSize: 30.0,
-                  color: Color(0xff21A177),
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: Get.height * 0.05),
-            TextField(
-              controller: controller.nameController,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.person_outline),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(color: Color(0xffE6FBDA)),
-                ),
-                labelText: 'Enter Your Name',
-                prefixIconColor: const Color(0xff20A479),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: Get.height * 0.2),
+              const Text(
+                'Register to get started',
+                style: TextStyle(
+                    fontSize: 30.0,
+                    color: Color(0xff21A177),
+                    fontWeight: FontWeight.bold),
               ),
-            ),
-            SizedBox(height: Get.height * 0.02),
-            TextField(
-              controller: controller.mobileControllerRegister,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.phone_outlined),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(color: Colors.green),
+              SizedBox(height: Get.height * 0.05),
+              TextField(
+                controller: controller.nameController,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.person_outline),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(color: Color(0xffE6FBDA)),
+                  ),
+                  labelText: 'Enter Your Name',
+                  prefixIconColor: const Color(0xff20A479),
                 ),
-                labelText: 'Enter Mobile Number',
-                prefixIconColor: const Color(0xff20A479),
               ),
-            ),
-            SizedBox(height: Get.height * 0.02),
-            TextField(
-              controller: controller.emailController,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.mail_outline),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(color: Colors.green),
+              SizedBox(height: Get.height * 0.02),
+              TextField(
+                controller: controller.mobileControllerRegister,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.phone_outlined),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(color: Colors.green),
+                  ),
+                  labelText: 'Enter Mobile Number',
+                  prefixIconColor: const Color(0xff20A479),
                 ),
-                labelText: 'Enter Your Email',
-                prefixIconColor: const Color(0xff20A479),
               ),
-            ),
-            SizedBox(height: Get.height * 0.05),
-            controller.loading.value
-                ? const Center(
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.orange,
-                      strokeWidth: 4,
-                    ),
-                  )
-                : SizedBox(
-                    height: 50,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        controller.fetchRegister();
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(const Color(0xff41AC1D)),
+              SizedBox(height: Get.height * 0.02),
+              TextField(
+                controller: controller.emailController,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.mail_outline),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(color: Colors.green),
+                  ),
+                  labelText: 'Enter Your Email',
+                  prefixIconColor: const Color(0xff20A479),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.05),
+              controller.loading.value
+                  ? const Center(
+                      child: CircularProgressIndicator(
+                        backgroundColor: Colors.orange,
+                        strokeWidth: 4,
                       ),
-                      child: const Text(
-                        'Register',
-                        style: TextStyle(fontSize: 18),
+                    )
+                  : SizedBox(
+                      height: 50,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          controller.fetchRegister();
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              const Color(0xff41AC1D)),
+                        ),
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Already have Account?',
+                    style: TextStyle(color: Color(0xff61B046), fontSize: 18),
+                  ),
+                  SizedBox(height: Get.height * 0.05),
+                  InkWell(
+                    onTap: () {
+                      Get.to(() => const LoginScreen());
+                    },
+                    child: const Text(
+                      ' Sign In',
+                      style: TextStyle(
+                        color: Color(0xff23A477),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
                       ),
                     ),
                   ),
-            const SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Already have Account?',
-                  style: TextStyle(color: Color(0xff61B046), fontSize: 18),
-                ),
-                SizedBox(height: Get.height * 0.05),
-                InkWell(
-                  onTap: () {
-                    Get.to(() => const LoginScreen());
-                  },
-                  child: const Text(
-                    ' Sign In',
-                    style: TextStyle(
-                      color: Color(0xff23A477),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
